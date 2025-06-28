87eb55c4331206bdbe4024b10e870000f1cb3096
fneco <izumi.f.tech@gmail.com>
2025-06-29 01:31:00 +0900

    refactor: enhance _relToAbs function to accept additional paths for resolution

--- src/relToAbs.ts
+++ src/relToAbs.ts
@@ -2,13 +2,17 @@ import { dirname, resolve } from "path";
 import { purry } from "remeda";
 import { fileURLToPath } from "url";
 
-const _relToAbs = (relativePath: string, fileUrl: string): string => {
+const _relToAbs = (
+  relativePath: string,
+  fileUrl: string,
+  paths: string[] = []
+): string => {
   const __filename = fileURLToPath(fileUrl);
   const __dirname = dirname(__filename);
-  return resolve(__dirname, relativePath);
+  return resolve(__dirname, ...(paths ?? []), relativePath);
 };
 
 export const relToAbs: {
-  (relativePath: string, fileUrl: string): string;
-  (fileUrl: string): (relativePath: string) => string;
+  (relativePath: string, fileUrl: string, paths?: string[]): string;
+  (fileUrl: string, paths?: string[]): (relativePath: string) => string;
 } = (...args: unknown[]) => purry(_relToAbs, args) as any;
