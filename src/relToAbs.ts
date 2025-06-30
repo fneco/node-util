93b7635ec3a931b548f60192cb97c3e0c6f7f7a4
fneco <izumi.f.tech@gmail.com>
2025-07-01 00:52:45 +0900

    Revert to version 0.0.8

--- src/relToAbs.ts
+++ src/relToAbs.ts
@@ -4,19 +4,15 @@ import { fileURLToPath } from "url";
 
 const _relToAbs = (
   relativePath: string,
-  fileUrl?: string,
+  fileUrl: string,
   paths: string[] = []
 ): string => {
-  if (!fileUrl) {
-    // return relativePath as absolute path if no fileUrl is provided
-    return relativePath;
-  }
   const __filename = fileURLToPath(fileUrl);
   const __dirname = dirname(__filename);
   return resolve(__dirname, ...(paths ?? []), relativePath);
 };
 
 export const relToAbs: {
-  (relativePath: string, fileUrl?: string, paths?: string[]): string;
-  (fileUrl?: string, paths?: string[]): (relativePath: string) => string;
+  (relativePath: string, fileUrl: string, paths?: string[]): string;
+  (fileUrl: string, paths?: string[]): (relativePath: string) => string;
 } = (...args: unknown[]) => purry(_relToAbs, args) as any;
