eda8f0d8df1e99f66bddf2f0aadaae3d941923ef
fneco <izumi.f.tech@gmail.com>
2025-06-30 23:56:28 +0900

    chore: update version to 0.0.7 in package.json and make options parameter optional in makeDirFileSync function

--- src/makeDirFileSync.ts
+++ src/makeDirFileSync.ts
@@ -5,9 +5,9 @@ import { relToAbs } from "./relToAbs";
 
 const _makeDirFileSync = (
   data: { filePath: string; content?: string },
-  options: { metaUrl?: string; encoding?: BufferEncoding }
+  options?: { metaUrl?: string; encoding?: BufferEncoding }
 ) => {
-  const { metaUrl, encoding = "utf8" } = options;
+  const { metaUrl, encoding = "utf8" } = options ?? {};
   const absFilePath = metaUrl
     ? relToAbs(data.filePath, metaUrl)
     : data.filePath;
@@ -18,10 +18,10 @@ const _makeDirFileSync = (
 
 export function makeDirFileSync(
   data: { filePath: string; content?: string },
-  option: { metaUrl?: string; encoding?: BufferEncoding }
+  option?: { metaUrl?: string; encoding?: BufferEncoding }
 ): void;
 
-export function makeDirFileSync(option: {
+export function makeDirFileSync(option?: {
   metaUrl?: string;
   encoding?: BufferEncoding;
 }): (data: { filePath: string; content?: string }) => void;
