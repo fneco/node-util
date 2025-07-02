18dd048a30c16a7870d2604500f2694fd07e6663
fneco <izumi.f.tech@gmail.com>
2025-07-02 21:13:27 +0900

    refactor: simplify relToAbs function by removing purry and unused parameters

--- src/relToAbs.ts
+++ src/relToAbs.ts
@@ -1,18 +1,16 @@
 import { dirname, resolve } from "path";
-import { purry } from "remeda";
 import { fileURLToPath } from "url";
 
-const _relToAbs = (
-  relativePath: string,
-  fileUrl: string,
+export const relToAbs = (
+  path: string,
+  fileUrl?: string,
   paths: string[] = []
 ): string => {
+  if (!fileUrl) {
+    // return path as absolute path if no fileUrl is provided
+    return path;
+  }
   const __filename = fileURLToPath(fileUrl);
   const __dirname = dirname(__filename);
-  return resolve(__dirname, ...(paths ?? []), relativePath);
+  return resolve(__dirname, ...(paths ?? []), path);
 };
-
-export const relToAbs: {
-  (relativePath: string, fileUrl: string, paths?: string[]): string;
-  (fileUrl: string, paths?: string[]): (relativePath: string) => string;
-} = (...args: unknown[]) => purry(_relToAbs, args) as any;
